57e0f5bc31d7d57d5a0d22091d4151c7fd07644c
xxccmd <a564268706@outlook.com>
2017-12-26 11:19:32 +0800

    注册页面

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
@@ -1,8 +1,57 @@
 package cn.edu.gdmec.android.boxuegu.activity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import cn.edu.gdmec.android.boxuegu.MainActivity;
+import cn.edu.gdmec.android.boxuegu.R;
+
 /**
  * Created by student on 17/12/25.
  */
 
-public class SplashActivity {
+public class SplashActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //设置为界面竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
+    }
+
+    private void init() {
+        TextView tv_version = (TextView) findViewById(R.id.tv_version);
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),0);
+            tv_version.setText("V"+info.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            tv_version.setText("V");
+        }
+
+        //利用timer三秒后执行
+        Timer timer = new Timer();
+        //timertask实现runnable接口
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+        timer.schedule(task,3000);//设置这个task在三秒后自动执行
+
+
+    }
 }
