ec4dc8a46c3cc5692aa1cf7580701ffe2a17384a
xxccmd <a564268706@outlook.com>
2017-12-28 11:48:51 +0800

    设置和修改密码完成和(设置密保和找回密码未完成)

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/FindPswActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/FindPswActivity.java
@@ -1,8 +1,15 @@
 package cn.edu.gdmec.android.boxuegu.activity;
 
-/**
- * Created by student on 17/12/28.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class FindPswActivity {
+import cn.edu.gdmec.android.boxuegu.R;
+
+public class FindPswActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_psw);
+    }
 }
